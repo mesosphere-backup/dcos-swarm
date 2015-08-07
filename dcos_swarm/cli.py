054cd3632fcef12379d6338a4dea7bf1ff4bff86
Timothy Chen <tnachen@gmail.com>
2015-08-07 16:22:11 -0700

    Allow setting up dcos swarm env

--- dcos_swarm/cli.py
+++ dcos_swarm/cli.py
@@ -5,7 +5,7 @@ Usage:
     dcos swarm --info
     dcos swarm --version
     dcos swarm --config-schema
-    dcos swarm daemon
+    dcos swarm env
 
 Options:
     --help                  Show this screen
@@ -16,6 +16,7 @@ from __future__ import print_function
 import docopt
 import os
 from dcos_swarm import constants, discovery
+from dcos import marathon, util
 
 
 def swarm_daemon():
@@ -23,6 +24,19 @@ def swarm_daemon():
 
 def print_schema():
     print("{}")
+
+def print_env():
+    url = util.get_config().get('core.dcos_url')
+    if url.startswith("http://"):
+       url = url[7:]
+    else:
+       return 1;
+
+    if url.endswith("/"):
+        url = url[:-1]
+
+    print("export DOCKER_HOST=" + url + ":80/service/swarm/")
+    return 0;
 
 def main():
     args = docopt.docopt(
@@ -33,8 +47,8 @@ def main():
         print(__doc__.split('\n')[0])
     elif args['--config-schema']:
         print_schema()
-    elif args['daemon']:
-        print("DOCKER_HOST=" + swarm_daemon())
+    elif args['env']:
+        return print_env()
     else:
         print(__doc__)
         return 1
